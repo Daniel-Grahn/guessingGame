7404b54aad3e044ad373a3585f1639c8e197dda5
root <root@Danielsbrbara>
2024-10-24 17:56:01 +0200

    Made two method calls (rule() and doGuesses()) in Guesser.java and added start() to the main method

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,5 +2,6 @@ public class GuessingGame {
     public static void main(String[] args) {
         Guesser theGuess = new Guesser();
         theGuess.Guesser(0,1000);
+        theGuess.start();
     }
 }
